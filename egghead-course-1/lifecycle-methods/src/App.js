63296d999d5776c408aa4e46e25247f088e3565b
Will Tinney <willtinney1@gmail.com>
2017-04-03 20:25:11 +0100

    Basic Mounting and unmounting

---
+++ egghead-course-1/lifecycle-methods/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {val: 0}
+    this.update = this.update.bind(this)
+  }
+  update() {
+    this.setState({val: this.state.val + 1})
+  }
+  componentWillMount(){
+    console.log('componentWillMount');
+    this.setState({m: 2})
+  }
+  componentDidMount(){
+    console.log('componentDidMount');
+    console.log(ReactDOM.findDOMNode(this));
+    this.inc = setInterval(this.update,500)
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+    clearInterval(this.inc)
+  }
+  render() {
+    console.log('render');
+    return (
+      <button onClick={this.update}>{this.state.val * this.state.m}</button>
+    );
+  }
+}
+
+class Wrapper extends Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id='a'></div>
+      </div>
+    )
+  }
+}
+
+export default Wrapper;
