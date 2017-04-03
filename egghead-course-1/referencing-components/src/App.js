56f9e56fe6b6d326f2d927a8ae078b4c91157508
Will Tinney <willtinney1@gmail.com>
2017-04-03 19:17:38 +0100

    referencing components

---
+++ egghead-course-1/referencing-components/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {a: ''}
+  }
+  update() {
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Input
+          ref={ component => this.a = component }
+          onChange={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
+      </div>
+    );
+  }
+}
+
+class Input extends Component {
+  render() {
+    return <input ref="input" type="text" onChange={this.props.update} />
+  }
+}
+
+export default App;
